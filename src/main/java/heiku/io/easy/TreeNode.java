77feb2fca07afafec731261c21b21207b9027e2b
Heiku <1154034741@qq.com>
2019-08-03 01:15:24 +0800

    2019/8/3

---
+++ src/main/java/heiku/io/easy/TreeNode.java
@@ -0,0 +1,12 @@
+package heiku.io.easy;
+
+/**
+ * @Author: Heiku
+ * @Date: 2019/8/3
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
