227accc0d541b7c40767293084a03868816ac919
Heiku <1154034741@qq.com>
2019-08-04 00:59:38 +0800

    2019/8/4

---
+++ src/main/java/heiku/io/easy/FlipAndInvertImage.java
@@ -0,0 +1,78 @@
+package heiku.io.easy;
+
+import java.util.Stack;
+
+/**
+ * @Author: Heiku
+ * @Date: 2019/8/3
+ *
+ * 832. Flipping an Image
+ *
+ * Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
+ *
+ * To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+ *
+ * To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
+ *
+ * Example 1:
+ *
+ *      Input: [[1,1,0],[1,0,1],[0,0,0]]
+ *      Output: [[1,0,0],[0,1,0],[1,1,1]]
+ *      Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+ *      Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+ *
+ * Example 2:
+ *
+ *      Input: [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+ *      Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ *      Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+ *      Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ *
+ */
+public class FlipAndInvertImage {
+
+    // binary loop update all arr
+    // if head == tail, them head = tail = 1 ^ head
+    // if head != tail, do not deal it
+    public int[][] flipAndInvertImage(int[][] A) {
+
+        for (int i = 0; i < A.length; i++){
+
+            int lo = 0, hi = A[0].length - 1;
+
+            while (lo <= hi){
+
+                if (A[i][lo] == A[i][hi]){
+                    A[i][lo] = 1 - A[i][lo];
+                    A[i][hi] = A[i][lo];
+                }
+                lo++;
+                hi--;
+            }
+        }
+        return A;
+    }
+
+
+    // deal with stack
+    public int[][] flipAndInvertImage2(int[][] A) {
+
+        for (int i = 0; i < A.length; i++){
+
+            Stack<Integer> stack = new Stack<>();
+            for (int j = 0; j < A[0].length; j++){
+                stack.add(A[i][j]);
+            }
+
+            for (int j = 0; j < A[0].length; j++){
+                int k = stack.pop();
+
+                A[i][j] = 1- k;
+            }
+
+        }
+
+
+        return A;
+    }
+}
---
+++ src/main/java/heiku/io/easy/SortArrayByParity.java
@@ -0,0 +1,72 @@
+package heiku.io.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: Heiku
+ * @Date: 2019/8/4
+ *
+ * 905. Sort Array By Parity
+ *
+ * Given an array A of non-negative integers, return an array consisting of all the even elements of A,
+ * followed by all the odd elements of A.
+ *
+ * You may return any answer array that satisfies this condition.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *      Input: [3,1,2,4]
+ *      Output: [2,4,3,1]
+ *
+ * The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+ */
+public class SortArrayByParity {
+
+
+    // O(n)
+    // loop i record even
+    // swap the value start the head of array
+    public int[] sortArrayByParity(int[] A) {
+        for (int i = 0, j = 0; i < A.length; i++) {
+            if (A[i] % 2 == 0){
+                int tmp = A[j];
+                A[j++] = A[i];
+                A[i] = tmp;
+            }
+        }
+        return A;
+    }
+
+
+
+    // use collection
+    // bad speed test : 2ms 17%
+    public int[] sortArrayByParity2(int[] A) {
+        List<Integer> evenArr = new ArrayList<>();
+        List<Integer> oddArr = new ArrayList<>();
+
+        for (int i : A) {
+            if (i % 2 == 0){
+                evenArr.add(i);
+            }else {
+                oddArr.add(i);
+            }
+        }
+
+        evenArr.addAll(oddArr);
+        A = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = evenArr.get(i);
+        }
+        return A;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr= new int[]{0, 2};
+        new SortArrayByParity().sortArrayByParity(arr);
+    }
+}
