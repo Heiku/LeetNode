dd6dfdf4cf563483e372ef73d5ecba03f103c60b
Heiku <1154034741@qq.com>
2019-08-09 00:26:11 +0800

    617

--- src/main/java/heiku/io/easy/MergeTrees.java
+++ src/main/java/heiku/io/easy/MergeTrees.java
@@ -29,19 +29,44 @@ package heiku.io.easy;
  */
 public class MergeTrees {
 
+    // add tree node into t1
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null){
+            return t2;
+        }
+        if (t2 == null){
+            return t1;
+        }
+        t1.val += t2.val;
+
+        mergeTrees2(t1.left, t2.left);
+        mergeTrees2(t1.right, t2.right);
+
+        return t1;
+    }
+
+
+
+    // build a new tree
+    // tree traversal
+    // Runtime: 1 ms, faster than 56.02% of Java online
+    public TreeNode mergeTrees2(TreeNode t1, TreeNode t2) {
         if (t1 == null && t2 == null){
             return null;
         }
-       if (t1 == null){
-           t1 = new TreeNode(t2.val);
-       }
-       if (t2 == null){
-           return t1;
-       }
-       mergeTrees(t1.left, t2.left);
-       mergeTrees(t1.right, t2.right);
 
-       return t1;
+        TreeNode tn;
+        if (t1 == null) {
+            tn = new TreeNode(t2.val);
+        }else if (t2 == null){
+            tn = new TreeNode(t1.val);
+        }else {
+            tn = new TreeNode(t1.val + t2.val);
+        }
+
+        tn.left = mergeTrees(t1 == null? null : t1.left, t2 == null ? null : t2.left);
+        tn.right = mergeTrees(t1 == null ? null : t1.right, t2 == null? null : t2.right);
+
+        return tn;
     }
 }
